89fefdae7c03e203264389c1e27d439fb2187993
Mark Wissink <mwissink.b@gmail.com>
2018-09-26 01:12:53 -0400

    first commit

---
+++ app/src/main/java/pilot/cs262/calvin/edu/knightrank/MainActivity.java
@@ -0,0 +1,13 @@
+package pilot.cs262.calvin.edu.knightrank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
