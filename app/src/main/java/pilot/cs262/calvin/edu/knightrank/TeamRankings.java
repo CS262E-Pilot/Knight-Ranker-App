8a20d56eb6c0cc1a58580d889863f0078e673734
Joseph Jinn <sorcere666@gmail.com>
2018-09-28 20:08:44 -0400

    Updated Prototype - Drawer Navigation, conversion of activities to Fragments for use with drawer navigation.
    - Converted ChallengeResults.java and MyRankings.java to Fragments
    
    -Implemented basic Drawer Navigation in Team Rankings activity.
    
    -Using Team Rankings activity as the host for ChallengeResults and MyRankings.
    
    -TeamRankings.java and activity_team_rankings.xml now contain code to use fragments.
    
    -App prototype is still perfectly functional.  The Drawer navigation, however, currently does not load any other screens.
    
    FIXME: Doesn't seem to be properly switching to any fragments.
    
    Note: Commented out code is in case I need to revert something or implement something at a later time.
    
    TODO: Figure out why the fragments aren't properly loading.
    TODO: Refactor code in TeamRankings.java once things are working.
    
    Resources Used:
    
    https://developer.android.com/training/implementing-navigation/nav-drawer
    https://developer.android.com/guide/components/fragments
    
    https://www.simplifiedcoding.net/android-navigation-drawer-example-using-fragments/
    https://guides.codepath.com/android/fragment-navigation-drawer
    
    P.S.:  Will continue working on this.

--- app/src/main/java/pilot/cs262/calvin/edu/knightrank/TeamRankings.java
+++ app/src/main/java/pilot/cs262/calvin/edu/knightrank/TeamRankings.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TeamRankings extends AppCompatActivity {
 
@@ -18,58 +27,15 @@ public class TeamRankings extends AppCompatActivity {
             TeamRankings.class.getSimpleName();
 
     private TextView mTextMessage;
-
-    // Set-up for bottom navigation bar.
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.ActivitySelection:
-                    mTextMessage.setText(R.string.title_activity_selection);
-
-                    // Launches the activity upon selection.
-                    Intent intent1 = new Intent(getApplicationContext(), ActivitySelection.class);
-
-                    startActivity(intent1);
-
-                    Log.d(LOG_TAG, "Bottom Bar Navigation Item Selected");
-
-                    return true;
-                case R.id.MyRankings:
-                    mTextMessage.setText(R.string.title_my_rankings);
-
-                    // Launches the activity upon selection.
-                    Intent intent2 = new Intent(getApplicationContext(), MyRankings.class);
-
-                    startActivity(intent2);
-
-                    Log.d(LOG_TAG, "Bottom Bar Navigation Item Selected");
-
-                    return true;
-                case R.id.Challenge:
-                    mTextMessage.setText(R.string.title_challenge);
-
-                    // Launches the activity upon selection.
-                    Intent intent3 = new Intent(getApplicationContext(), ChallengeResults.class);
-
-                    startActivity(intent3);
-
-                    Log.d(LOG_TAG, "Bottom Bar Navigation Item Selected");
-                    return true;
-            }
-            return false;
-        }
-    };
+    private DrawerLayout mDrawerLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_rankings);
 
         // my_child_toolbar is defined in the layout file
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
         // Get a support ActionBar corresponding to this toolbar
@@ -78,10 +44,131 @@ public class TeamRankings extends AppCompatActivity {
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
-        // Bottom Navigation Bar
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // Custom icon for the Up button
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        // Set-up for the drawer navigation.
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        // Create a new fragment and specify the fragment to show based on nav item clicked
+                        Fragment fragment = null;
+                        Class fragmentClass;
+
+                        // To handle selection of menu items.
+                        switch (menuItem.getItemId()) {
+//                            case R.id.nav_activity_selection:
+//                                fragmentClass = ActivitySelection.class;
+//                                Log.e(LOG_TAG, "Selected the activity selection activity!");
+//                                break;
+//                            case R.id.nav_team_rankings:
+//                                fragmentClass = TeamRankings.class;
+//                                Log.e(LOG_TAG, "Selected the team rankings activity!");
+//                                break;
+                            case R.id.nav_my_rankings:
+                                fragmentClass = MyRankings.class;
+                                Log.e(LOG_TAG, "Selected the my rankings fragment!");
+                                break;
+                            case R.id.nav_challenge_results:
+                                fragmentClass = ChallengeResults.class;
+                                Log.e(LOG_TAG, "Selected the challenge results fragment!");
+                                //Toast.makeText(getApplicationContext(), "Selected", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                fragmentClass = ActivitySelection.class;
+                        }
+
+                        // Check that the activity is using the layout version with
+                        // the fragment_container FrameLayout
+                        if (findViewById(R.id.fragment_content) != null) {
+
+                            // However, if we're being restored from a previous state,
+                            // then we don't need to do anything and should return or else
+                            // we could end up with overlapping fragments.
+                            if (savedInstanceState != null) {
+                                return false;
+                            }
+                        }
+
+                        // Create a new Fragment.
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        // In case this activity was started with special instructions from an
+                        // Intent, pass the Intent's extras to the fragment as arguments
+                        try {
+                            fragment.setArguments(getIntent().getExtras());
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                        // Necessary for fragments.
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                        // Replace whatever is in the fragment_container view with this fragment,
+                        // and add the transaction to the back stack so the user can navigate back
+                        transaction.replace(R.id.fragment_content, fragment).commit();
+                        transaction.addToBackStack(null);
+
+                        // Commit the transaction
+                        transaction.commit();
+
+
+                        // Highlight the selected item has been done by NavigationView
+                        menuItem.setChecked(true);
+                        // Set action bar title
+                        setTitle(menuItem.getTitle());
+                        // Close the navigation drawer
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+        // Placeholder - in case we want to do something with drawer states.
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
