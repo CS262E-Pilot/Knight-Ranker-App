3540205f98380bfa368d54ac0ab357c274e2e8d4
mrsillydog <ian.sam.adams@gmail.com>
2018-10-04 15:14:37 -0400

    Account Creation
    Updated Account Creation Activity UI and implemented minor Account username/pass requirements.

--- app/src/main/java/pilot/cs262/calvin/edu/knightrank/AccountCreation.java
+++ app/src/main/java/pilot/cs262/calvin/edu/knightrank/AccountCreation.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AccountCreation extends AppCompatActivity {
 
@@ -14,10 +16,17 @@ public class AccountCreation extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
 
+    private EditText username;
+    private EditText password;
+    private EditText confirmPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
+        username = (EditText) findViewById(R.id.editText_username);
+        password = (EditText) findViewById(R.id.editText_password);
+        confirmPassword = (EditText) findViewById(R.id.editText_confirm_Password);
 
         // my_child_toolbar is defined in the layout file
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
@@ -44,5 +53,28 @@ public class AccountCreation extends AppCompatActivity {
         startActivity(intent);
 
         Log.d(LOG_TAG, "Button clicked!");
+    }
+
+    public void createAccount(View view) {
+        String user_name = username.getText().toString();
+        String user_password = password.getText().toString();
+        String user_confirmed_password = confirmPassword.getText().toString();
+
+        if(!user_name.equals("")) {
+            if(user_password.equals(user_confirmed_password)) {
+                Intent intent = new Intent(this, MainActivity.class);
+
+                startActivity(intent);
+
+                Log.d(LOG_TAG, "Button clicked!");
+                Toast.makeText(getApplicationContext(), "Account Created!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Passwords must match.", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Username must not be blank.", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
