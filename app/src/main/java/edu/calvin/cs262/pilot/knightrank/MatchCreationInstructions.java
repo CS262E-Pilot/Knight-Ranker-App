355639a9652c2ecd172884ede03ced932bbeed67
Joseph Jinn <sorcere666@gmail.com>
2018-12-06 21:42:39 -0500

    Implemented Create Match and Confirm Match Instructions...
    -Oops, committed without staging all the relevant files.
    
    -Added MatchCreationInstructions and MatchConfirmationInstructions .java fragments files.
    
    -Added technical instructions with screen captures.
    
    -Added screen captures as .png files in "drawable" resource folder.
    
    Note: I will leave it up to the team members who wrote these instructions to update them if necessary, otherwise
    will leave them as is.
    
    TODO: Update instructions to reflect current state of the UI and app functionality.
    
    TODO: Implement further fragments detailing technical instructions on other app functionality.
    
    FIXME: formatting tweaks, if have time.

--- app/src/main/java/edu/calvin/cs262/pilot/knightrank/MatchCreationInstructions.java
+++ app/src/main/java/edu/calvin/cs262/pilot/knightrank/MatchCreationInstructions.java
@@ -1,12 +1,20 @@
 package edu.calvin.cs262.pilot.knightrank;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import java.util.Objects;
 
 
 /**
@@ -22,6 +30,21 @@ public class MatchCreationInstructions extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    //Class variables.
+    private static final String LOG_TAG =
+            MatchCreationInstructions.class.getSimpleName();
+
+    // For use with shared preferences.
+    private static String PLACEHOLDER6 = "";
+
+    // Share preferences file (custom)
+    private SharedPreferences mPreferences;
+    // Shared preferences file (default)
+    private SharedPreferences mPreferencesDefault;
+
+    // Name of the custom shared preferences file.
+    private static final String sharedPrefFile = "pilot.cs262.calvin.edu.knightrank";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,6 +81,18 @@ public class MatchCreationInstructions extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        // Set shared preferences component.
+        // Note: modified from the one in activities as this is a fragment.
+        mPreferences = Objects.requireNonNull(this.getActivity()).getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE);
+        mPreferencesDefault = android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+
+        // Placeholder code as example of how to get values from the default SharedPrefs file.
+        String syncFreq = mPreferencesDefault.getString(SettingsActivity.KEY_SYNC_FREQUENCY, "-1");
+
+        // Placeholder code as example of how to restore values to UI components from shared preferences.
+        //username_main.setText(mPreferences.getString(USER_NAME, ""));
+        //password_main.setText(mPreferences.getString(USER_PASSWORD, ""));
     }
 
     @Override
@@ -72,6 +107,26 @@ public class MatchCreationInstructions extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
+    }
+
+    /**
+     * Method implemented to change background color programmatically via color picket.
+     *
+     * @param view object
+     * @param savedInstanceState object
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Change the background color to what was selected in color picker.
+        // Note: Change color by using findViewById and ID of the UI element you wish to change.
+        RelativeLayout thisLayout = Objects.requireNonNull(getView()).findViewById(R.id.fragment_match_creation_instructions_root_layout);
+        thisLayout.setBackgroundColor(mPreferences.getInt(ColorPicker.APP_BACKGROUND_COLOR_ARGB, Color.WHITE));
+
+        int value = mPreferences.getInt(ColorPicker.APP_BACKGROUND_COLOR_ARGB, Color.BLACK);
+
+        Log.e(LOG_TAG,"Value of color is: " + value);
     }
 
     @Override
